007ef8669b96836ced17593086df19f923b101d2
yehe <halonein@hotmail.com>
2021-04-02 10:42:00 +0800

    mini

--- leetcode/src/189-Rotate Array.java
+++ leetcode/src/189-Rotate Array.java
@@ -1,44 +1,6 @@
 import java.util.Arrays;
 
 class RotateArray {
-    public static int maxProfit(int[] prices) {
-        int n = prices.length;
-
-        int p = 0,
-            q = 0;
-        int sum = 0;
-        boolean up = false;
-        for(int i = 0; i < n-1; i++) {
-            if(prices[i+1] > prices[i]){
-                up = true;
-                q = i+1;
-            }
-            else if(prices[i+1] == prices[i]){
-                if(up){
-                    q = i+1;
-                }
-                else{
-                    p = i+1;
-                }
-            }
-            else{
-                if(up){
-                    up = false;
-                    sum += prices[q] - prices[p];
-                    p = i+1;
-                }
-                else{
-                    p = i+1;
-                }
-            }
-        }
-        if(up){
-            sum += prices[q] - prices[p];
-        }
-        return sum;
-    }
-
-
     public static void rotate(int[] nums, int k) {
         var i = k-1;
         var n = nums.length;
@@ -84,10 +46,6 @@ class RotateArray {
     }
 
     public static void main(String[] args) {
-        int[] a = {7,1,5,3,6,4};
-        int ans = maxProfit(a);
-        System.out.println(ans);
-
 
 
         int[] b = {-1,-100,3,99};
